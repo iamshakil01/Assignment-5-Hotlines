7e7410b09fe34003bc3586fd10ad51056a955040
Your Name <you@example.com>
2025-08-29 10:08:25 +0600

    added alert function of all call buttons

--- script.js
+++ script.js
@@ -3,16 +3,39 @@ function getElement(id){
     return element;
 }
 
-let totalHearts = 0;
+let totalHearts = 1;
  const cardButtons = document.getElementsByClassName("heart-btn")
 
 for(let cardButton of cardButtons){
     cardButton.addEventListener("click", function(){
         let heart =document.getElementById("hearts").innerText = totalHearts++
-        console.log(heart)
     })
 }
 
+// Call Buttons Function
+
+// const callButtons = document.getElementsByClassName("call-button")
+
+// for(callButton of callButtons){
+//     callButton.addEventListener("click", function(){
+//         alert(`Calling ${serviceName} number ${serviceNumber}...`);
+//     })
+// }
+
+
+ const callButtons = document.querySelectorAll('.call-button');
+
+    for(const button of callButtons){
+    button.addEventListener('click',function() {
+    const card = this.closest('.card');
+    this.style = 'black';
+    if (!card) return;
+    const title = card.querySelector('.title').textContent;
+    const number = card.querySelector('.number').textContent;
+
+    alert(`Calling ${title} number ${number}...`);
+    });
+  }
 
 
 
